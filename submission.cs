49f19d35628926a777c0a10bcd221ace1803c949
Dmytro Ohorodniichuk <ohorodniichuk@gmail.com>
2025-10-26 14:10:52 +0200

    Add submission.cs and update solution file

---
+++ submission.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
+namespace ConsoleApp1
+{
+
+
+    public class Program
+    {
+        // These URLs will be read by the autograder, please keep the variable name un-changed and link to the correct xml/xsd files.
+        public static string xmlURL = "https://www.public.asu.edu/1238603420/Hotels.xml"; //Q1.2
+        public static string xmlErrorURL = "https://www.public.asu.edu/1238603420/HotelsErrors.xml"; //Q1.3
+        public static string xsdURL = "https://www.public.asu.edu/1238603420/Hotels.xsd"; //Q1.1;
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+
+
+            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+            return "No Error";
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+
+
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            return "{}";
+
+        }
+    }
+
+}
